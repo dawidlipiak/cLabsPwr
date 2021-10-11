31d3f563aff4a10d94975956718a719394c8a9dc
dawidlipiak <dawidlipiak@gmail.com>
2021-10-11 19:51:25 +0200

    zadanie lista 1

---
+++ lista1/zad4.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main(void)
+{
+    //wysokosc trojkata z gwiazdek
+    int n;
+
+    scanf("%d",&n);
+
+    //poczatkowa wartosc wielkosci przerwy do gwiazdki
+    int space = n-1;
+
+    //ilosc wyswietlanych gwiazdek w wierszu
+    int stars = 1;
+
+    //wyswietlanie gwiazdek
+    for(int i = 0; i < n; i++)
+    {
+        //wyswietlenie przerwy przed gwiazdka
+        for(int j = 0; j < space; j++)
+        {
+            printf(" ");
+        }
+
+        //wyswietlenie gwaizdek w wierszu
+        for(int k = 0; k < stars; k++)
+        {
+            printf("*");
+        }
+
+        space--;
+        stars += 2;
+        printf("\n");
+    }
+
+    return 0;
+}
+
+
