17a41dc34c603a246c1edf3b13d7aab9d2cbf7de
dawidlipiak <dawidlipiak@gmail.com>
2021-10-26 14:28:27 +0200

    zadania z listy2

--- lista2/zad2.c
+++ lista2/zad2.c
@@ -7,13 +7,21 @@ int main (void)
     int suma = 0;
 
     scanf ("%d", &n);
-
+    //sprawdzenie czy zostala podana liczba argumentow 0
+    if (n==0)
+    {
+        printf("nie mozna wyliczyc sredniej arytmetycznej dla 0 liczby danych\n");
+    	return 0;
+    }
+    
+    //zczytywanie danych do obliczania sredniej i wyliczenie ich sumy
     for( int i = 0; i < n; i++ )
     {
         scanf("%d", &x);
         suma += x;
     }
 
+    //wyswietlenie wyniku
     printf("\nsrednia arytmetyczna: %d\n", suma/n);
 
     return 0;
---
+++ lista2/zad3.c
@@ -0,0 +1,18 @@
+// Dawid Lipiak lista2 zadanie3 
+
+#include <stdio.h>
+
+int main (void)
+{
+    double wyrazenie = 1.0;
+    int n = 1;
+
+    while ( wyrazenie <= 10)
+    {
+        n++;
+        wyrazenie = wyrazenie + (double)1/n;
+    }
+ 
+    printf("%d", n);
+    return 0;
+}
